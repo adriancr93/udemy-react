0552fa371371e5d10e154dd916bcc8723e8ae160
adriancr93 <adriancr93@hotmail.com>
2025-05-13 13:00:44 -0600

    Funciones

---
+++ IntroduccionJS/js/17.js
@@ -0,0 +1,23 @@
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+const numeros = [10, 20, 30];
+
+//Filter
+// const nuevoArray = tecnologias.filter((tech) => tech !== 'HTML' );
+// const resultado = numeros.filter((numero) => numero > 15);
+
+// Includes
+// const resultado = tecnologias.includes('CSS');
+
+// Some - Devuelve si al menos uno cumple la condicion
+// const resultado = numeros.some((numero) => numero > 15);
+
+// Find - Devuelve el primer elemento que cumple  una funcion
+// const resultado = numeros.find(numero => numero > 20 );
+
+// Every - Retorna true o false si todos cumplen la condicion
+// const resultado = numeros.every(numero => numero > 5);
+
+// Reduce - Retorna un acumulado del total
+// const resultado = numeros.reduce( (total, numero) => total + numero, 0)
+
+console.log(resultado);
---
+++ IntroduccionJS/js/18.js
@@ -0,0 +1,12 @@
+// Condicionales
+
+const disponible = 4000
+const retirar = 200
+
+const auth = true
+
+if(auth) {
+    console.log('Acceso al sistema...');
+} else {
+    console.log('Acceso denegado');
+}
